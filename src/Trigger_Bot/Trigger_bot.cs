422172631cf38b8acb843ed3d8bc37b4a4ec930c
Kai Schinnen <s34kschi@uni-bonn.de>
2025-02-27 16:34:55 +0100

    outsorced offsets

--- src/Trigger_Bot/Trigger_bot.cs
+++ src/Trigger_Bot/Trigger_bot.cs
@@ -12,6 +12,14 @@ namespace TriggerBot
         public int team { get; set; }
         public uint m_lifeState { get; set; }
     }
+    public class Offsets {
+        public static int dwLocalPlayerPawn = 0x1889F20;
+        public static int dwEntityList = 0x1A359B0;
+        public static int m_iTeamNum = 0x3E3;
+        public static int m_lifeState = 0x348;
+        public static int m_iIDEntIndex = 0x1458;
+        public static int m_iHealth = 0x344;
+    }
 
     class Program
     {
@@ -19,13 +27,6 @@ namespace TriggerBot
         static Swed swed = new Swed("cs2");
         static IntPtr client = swed.GetModuleBase("client.dll");
         static IntPtr attack = client + 0x1882720;
-
-        static int dwLocalPlayerPawn = 0x1889F20;
-        static int dwEntityList = 0x1A359B0;
-        static int m_iTeamNum = 0x3E3;
-        static int m_lifeState = 0x348;
-        static int m_iIDEntIndex = 0x1458;
-        static int m_iHealth = 0x344;
         static void Main()
         {
             Entity localPlayer = new Entity();
@@ -35,7 +36,7 @@ namespace TriggerBot
                 Console.Clear();
                 
                 // get local player
-                localPlayer.pawnAddress = swed.ReadPointer(client, dwLocalPlayerPawn);
+                localPlayer.pawnAddress = swed.ReadPointer(client, Offsets.dwLocalPlayerPawn);
                 if (localPlayer.pawnAddress == IntPtr.Zero)
                 {
                     Thread.Sleep(10);
@@ -43,11 +44,11 @@ namespace TriggerBot
                 }
                 
                 // get local player team
-                localPlayer.team = swed.ReadInt(localPlayer.pawnAddress, m_iTeamNum);
+                localPlayer.team = swed.ReadInt(localPlayer.pawnAddress, Offsets.m_iTeamNum);
                 Console.WriteLine($"LocalPlayer Team: {localPlayer.team}");
 
                 // get crosshair entity
-                int targetEntityIndex = swed.ReadInt(localPlayer.pawnAddress, m_iIDEntIndex);
+                int targetEntityIndex = swed.ReadInt(localPlayer.pawnAddress, Offsets.m_iIDEntIndex);
                 Console.WriteLine($"Crosshair Entity Index: {targetEntityIndex}");
                 
                 // if crosshair is on no entity
@@ -58,7 +59,7 @@ namespace TriggerBot
                 }
 
                 // accessing entity list
-                IntPtr entityList = swed.ReadPointer(client, dwEntityList);
+                IntPtr entityList = swed.ReadPointer(client, Offsets.dwEntityList);
                 IntPtr listEntry = swed.ReadPointer(entityList, 0x8 * (targetEntityIndex >> 9) + 0x10); // apply bitmask 0x7FFF and shift bits by 9
                 IntPtr targetEntity = swed.ReadPointer(listEntry, 0x78 * (targetEntityIndex & 0x1FF));  // read current pawn, apply bitmask to stay in range
                 
@@ -69,9 +70,9 @@ namespace TriggerBot
                 }
                 
                 // get attributes
-                int targetTeam = swed.ReadInt(targetEntity, m_iTeamNum);
-                uint targetLifeState = swed.ReadUInt(targetEntity, m_lifeState);
-                int targetHealth = swed.ReadInt(targetEntity, m_iHealth);
+                int targetTeam = swed.ReadInt(targetEntity, Offsets.m_iTeamNum);
+                uint targetLifeState = swed.ReadUInt(targetEntity, Offsets.m_lifeState);
+                int targetHealth = swed.ReadInt(targetEntity, Offsets.m_iHealth);
 
                 Console.WriteLine($"Target Team: {targetTeam}, LifeState: {targetLifeState}, Health: {targetHealth}");
                 
