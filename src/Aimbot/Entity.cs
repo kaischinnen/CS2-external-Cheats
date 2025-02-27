cda1435609cdab6e0bc3a48b28c8a3dc4d889796
Kai Schinnen <s34kschi@uni-bonn.de>
2025-02-28 00:17:17 +0100

    b

--- src/Aimbot/Entity.cs
+++ src/Aimbot/Entity.cs
@@ -11,8 +11,10 @@ public class Entity {
     public IntPtr controllerAddress { get; set; }
     public Vector3 origin { get; set; }
     public Vector3 view { get; set; }
+    public Vector3 head { get; set; }
     public int health { get; set; }
     public int team { get; set; }
     public uint lifestate { get; set; }
     public float distance { get; set; } // from localPlayer
+    public string name { get; set; } = string.Empty;
 }
