0aac6b193420b934462e1706d35f3f57f4e87ad9
Kai Schinnen <s34kschi@uni-bonn.de>
2025-02-27 01:02:59 +0100

    pushed triggerbote file

--- src/Trigger_Bot/Trigger_bot.cs
+++ src/Trigger_Bot/Trigger_bot.cs
@@ -1,46 +1,29 @@
-﻿# cs2-dumper
+﻿using Swed64;
+using System.Net.WebSockets;
+using System.Runtime.InteropServices;
 
-An external offset/interface dumper for Counter-Strike 2, with support for both Windows & Linux. Powered
-by [memflow](https://github.com/memflow/memflow).
+Swed swed = new Swed("cs2");
 
-The native Linux version is available in the [linux](https://github.com/a2x/cs2-dumper/tree/linux) branch.
+IntPtr client = swed.GetModuleBase("client.dll");
 
-For a work-in-progress offline version, please check out [cs2-analyzer](https://github.com/a2x/cs2-analyzer) and the web demo here: https://a2x.github.io/cs2-analyzer
+IntPtr attack = client + 0x1882720; 
 
-## Getting Started
+while (true) {
+    Console.Clear();
 
-You can download the latest release from [Releases](https://github.com/a2x/cs2-dumper/releases) or compile it yourself.
-Note that compiling it yourself requires your Rust compiler version to be at least 1.74.0 or newer, and the nightly
-toolchain must be installed.
+    IntPtr localPlayerPawn = swed.ReadPointer(client, 0x1889F20); // dwLocalPlayerPawn
+    int entIndex = swed.ReadInt(localPlayerPawn, 0x1458); // m_iIDEntIndex
+    Console.WriteLine($"Crosshair/Entity ID: {entIndex}");
 
-## Usage
+    if (GetAsyncKeyState(0x06) < 0 )  { // mouse 4
+        if (entIndex > 0 ) {    // if *any* entity is in crosshair
+            swed.WriteInt(attack, 65537); // +attack
+            Thread.Sleep(1);
+            swed.WriteInt(attack, 256); // -attack
+        }
+    }
+    Thread.Sleep(1);
+}
 
-1. Ensure the game process is running (Being in the main menu should suffice).
-2. Run the `cs2-dumper` executable (Note that some memflow connectors may require elevated privileges).
-
-_Note:_ If you run the executable without specifying an optional memflow connector name, it will automatically use the
-[memflow-native](https://github.com/memflow/memflow-native) OS layer to read the memory of the game process. If you
-wish to use an existing memflow connector instead, you can pass the `connector` and optional `connector-args` arguments
-to the program.
-
-E.g. `sudo ./cs2-dumper -c pcileech -a :device=FPGA -vvv`
-
-### Available Arguments
-
-- `-c, --connector <connector>`: The name of the memflow connector to use.
-- `-a, --connector-args <connector-args>`: Additional arguments to pass to the memflow connector.
-- `-f, --file-types <file-types>`: The types of files to generate. Default: `cs`, `hpp`,  `json`, `rs`.
-- `-i, --indent-size <indent-size>`: The number of spaces to use per indentation level. Default: `4`.
-- `-o, --output <output>`: The output directory to write the generated files to. Default: `output`.
-- `-p, --process-name <process-name>`: The name of the game process. Default: `cs2.exe`.
-- `-v...`: Increase logging verbosity. Can be specified multiple times.
-- `-h, --help`: Print help.
-- `-V, --version`: Print version.
-
-## Running Tests
-
-To run tests, use the following command: `cargo test -- --nocapture`.
-
-## License
-
-Licensed under the MIT license ([LICENSE](./LICENSE)).
+[DllImport("user32.dll")]
+static extern short GetAsyncKeyState(int vKey); // hotkey
