dcdd2104ecb0fbe31590181733144403b15584e8
Kai Schinnen <s34kschi@uni-bonn.de>
2025-02-28 18:47:49 +0100

    updates offsets

---
+++ Downloads/Git/CS2-external-Cheats-main/src/Anti-Flash/Anti-Flash.cs
@@ -0,0 +1,25 @@
+﻿using Swed64;
+class AntiFlash {
+    static void Main() {
+        Swed swed = new Swed("cs2");
+
+        IntPtr client = swed.GetModuleBase("client.dll");
+
+        // offsets
+        int dwLocalPlayerPawn = 0x188AF10;
+        int m_flFlashBangTime = 0x13F8;
+
+        while (true) {
+            IntPtr localPlayerPawn = swed.ReadPointer(client, dwLocalPlayerPawn);
+
+            float flashDuration = swed.ReadFloat(localPlayerPawn, m_flFlashBangTime); 
+
+            if (flashDuration > 0) {
+                swed.WriteFloat(localPlayerPawn, m_flFlashBangTime, 0);
+                Console.WriteLine("Avoided Flash!"); 
+            }
+            Thread.Sleep(2);
+        }
+    }   
+
+}
