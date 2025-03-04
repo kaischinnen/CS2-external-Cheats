4d5bf007eeeacaf69a3af5995d8b7ee5eb4208c6
Kai Schinnen <s34kschi@uni-bonn.de>
2025-03-04 19:41:58 +0100

    deleted individual projects for each method

--- src/Aimbot/ViewMatrix.cs
+++
@@ -1,7 +0,0 @@
-﻿public class ViewMatrix // 4x4
-{
-    public float m11, m12, m13, m14;
-    public float m21, m22, m23, m24;
-    public float m31, m32, m33, m34;
-    public float m41, m42, m43, m44; 
-}
---
+++ src/allCheats/offsets.cs
@@ -0,0 +1,27 @@
+﻿public static class Offsets
+{
+    // offsets.cs
+    public const int dwViewAngles = 0x1AACA70;
+    public const int dwLocalPlayerPawn = 0x188AF20;
+    public const int dwEntityList = 0x1A36A00;
+    public const int dwViewMatrix = 0x1AA27F0;
+
+    // client.dll.cs
+    public const int m_hPlayerPawn = 0x80C;
+    public const int m_iHealth = 0x344;
+    public const int m_vOldOrigin = 0x1324;
+    public const int m_iTeamNum = 0x3E3;
+    public const int m_vecViewOffset = 0xCB0;
+    public const int m_lifeState = 0x348;
+    public const int m_modelState = 0x170;
+    public const int m_pGameSceneNode = 0x328;
+    public const int m_iszPlayerName = 0x660;
+    public const int m_flFlashBangTime = 0x13F8;
+    public const int m_bSpotted = 0x8;
+    public const int m_entitySpottedState = 0x23D0;
+    public const int m_iIDEntIndex = 0x1458;
+
+    // button.cs#
+    public const int jump = 0x1883C20;
+    public const int attack = 0x1883710;
+}
